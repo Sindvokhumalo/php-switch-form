4f1575a40c47d4a2ac6cfdee0358a3f9a2b128c3
Sindvokhumalo <khumalosindvo@gmail.com>
2023-01-25 01:59:23 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html>
+   <head>
+        <title></title>
+        <link rel="stylesheet" href="style.css">
+   </head>
+<body>
+        
+ <form method="post" action="switch.php">
+  <div>
+  <h2> GRADING SYSTEM</h2>
+
+    <label>1.Algorithms</label><br>
+    <label>2.Database</label><br>
+    <input class="data" id="txt" type="text" name="module" placeholder="MODULE: 1 OR 2">
+  </div>
+    <div>
+    <input class="data" id="txt" type="text" name="grade" placeholder="ENTER MARK">
+  </div>
+   <input class="submit" type="submit" value="submit">
+   </form>
+</body>
+</html>
+
---
+++ switch.php
@@ -0,0 +1,53 @@
+﻿<html>
+<body>
+
+<?php
+          $module=$_POST ['module'];
+          if($module==1){
+              echo "Your grade for the module is : Algorithms -";
+          }
+             elseif($module==2){
+                 echo "Your grade for the module is : Database -";
+             }
+                 else{
+                     echo "UNKNOWN MODULE AND  ";
+                 }
+             
+
+         
+                 $grade=$_POST['grade'];
+
+
+       switch($grade)
+           {
+        case ($grade>=80 && $grade<=100) :
+            echo "  Grade is A";
+        break;
+
+        case  ($grade>=70 && $grade<=79):
+             echo "  Grade is B";
+        break;
+
+        case  ($grade>=60 && $grade<=69):
+            echo "   Grade is C";
+        break;
+
+        case  ($grade>=50 && $grade<=59):
+            echo "   Grade is D";
+        break;
+       
+        case  ($grade>=40 && $grade<=49):
+            echo "   Grade is E";
+         break;
+        
+         case  ($grade>=0 && $grade<=39):
+            echo "   Grade is F";
+         break;
+
+        default:
+            echo  "  ENTER VALID MARK  ";
+}
+
+?>
+</body>
+</html>
